839d6a1514d41468c8e9290ff4685f52ce7f091e
vinit saboo <vinitsaboo90@gmail.com>
2023-03-05 13:25:18 +0000

    add floc off script

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,35 @@
-import "floc-off";
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import {router} from './routes/routes';
 import { RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import GameContextProvider from './context/gameLevelContext';
+
+
+function flocOff() {
+  const cohorts = "interestCohort";
+const documentProto:any = Document.prototype;
+const flocSupported = cohorts in documentProto;
+
+if (!flocSupported) {
+  return;
+}
+
+const descriptor = Object.getOwnPropertyDescriptor(documentProto, cohorts);
+const writable = descriptor && descriptor.writable;
+if (writable) {
+  const proxy = new Proxy(documentProto[cohorts], { apply: () => Promise.reject() });
+  const config = {
+    writable: false,
+    value: proxy,
+    configurable: false,
+    enumerable: false,
+  };
+  Object.defineProperty(documentProto, cohorts, config);
+}
+}
+
+flocOff();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
